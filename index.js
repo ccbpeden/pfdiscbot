ef28e53cb9ed7fa346680c2ec0ceb06da8d574bb
Charles Peden <ccbpeden@warpmail.net>
2021-05-25 19:07:48 -0700

    wrap it all in a try catch block

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const { Client } = require( 'discord.js' );
 const dotenv = require( 'dotenv' ).config();
 const client = new Client();
+const http = require( 'http' );
 
-console.log( 'prawcess: ', process.env );
-
-client.login( process.env.discordToken );
-
-client.on( 'ready', () => {
-	console.log( 'We are prepared to meet our maker.' );
-} );
+try {
+	http.createServer( ( request, response ) => {
+		response.writeHead(
+			200,
+			{ 'Content-type': 'text/plain' }
+		);
+		response.write( 'Hello World...' );
+		response.end();
+	} ).listen( process.env.port );
+	
+	console.log( 'prawcess: ', process.env );
+	
+	client.login( process.env.discordToken );
+	
+	client.on( 'ready', () => {
+		console.log( 'We are prepared to meet our maker.' );
+	} );
+} catch ( error ) {
+	console.log( 'error: ', error );
+	return error;
+}
