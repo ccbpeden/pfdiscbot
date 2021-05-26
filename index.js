a3897489f2065551d92f9e5679b8be541273baa2
Charles Peden <ccbpeden@warpmail.net>
2021-05-25 18:51:38 -0700

    add client

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const { Client, Intents } = require( 'discord.js' );
 const dotenv = require( 'dotenv' );
-const client = new Discord.client();
+const client = new Client();
+
+console.log( 'prawcess: ', process.env );
+
+client.login( process.env.discordToken );
+
+client.on( 'ready', () => {
+	console.log( 'We are prepared to meet our maker.' );
+} );
