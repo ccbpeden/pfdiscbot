445233adb907278fe5d8a792bf9ca8169c17891e
Charles Peden <ccbpeden@warpmail.net>
2021-05-30 14:55:20 -0700

    try armor

--- constants.js
+++ constants.js
@@ -4,6 +4,7 @@ module.exports = {
 		'!skill': { segment: 'Skills', selector: '#ctl00_MainContent_DataListTalentsAll' },
 		'!feat': { segment: 'FeatDisplay', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
 		'!spell': { segment: 'SpellDisplay', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
+		'!armor': { segment: 'EquipmentArmorDisplay', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
 	},
 	wordsNotToCapitalize: [
 		'of',
