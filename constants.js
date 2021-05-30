3f2a2fc34858b9771c6ca14a672ad32ebdada3f2
Charles Peden <ccbpeden@warpmail.net>
2021-05-30 14:43:05 -0700

    try for a more generous slector for ids

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@ module.exports = {
 	defaultQueryError: 'Sorry, I can\'t match your query.',
 	pageArgs: {
 		'!skill': { segment: 'Skills', selector: '#ctl00_MainContent_DataListTalentsAll' },
-		'!feat': { segment: 'FeatDisplay', selector: '#ctl00_MainContent_DataListTypes_ctl00_LabelName' },
+		'!feat': { segment: 'FeatDisplay', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
 		'!spell': { segment: 'SpellDisplay', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
 	},
 	wordsNotToCapitalize: [
