82227bbd92fd49052172e3b80181fecae975d619
Charles Peden <ccbpeden@warpmail.net>
2021-06-04 15:39:59 -0700

    Revert "add baseUrl to page args constants"
    This reverts commit f7307e6a28f269ca0c588cfe4bf506d8cc56f82d.

--- constants.js
+++ constants.js
@@ -1,17 +1,14 @@
-const firstEditionBaseUrl = 'https://aonprd.com/'
-const secondEditionBaseUrl = 'https://2e.aonprd.com/'
-
 module.exports = {
 	defaultQueryError: 'Sorry, I can\'t match your query.',
 	pageArgs: {
-		'!skill': { baseUrl: firstEditionBaseUrl, segment: 'Skills.aspx?ItemName=', selector: '#ctl00_MainContent_DataListTalentsAll' },
-		'!feat': { baseUrl: firstEditionBaseUrl, segment: 'FeatDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
-		'!spell': { baseUrl: firstEditionBaseUrl, segment: 'SpellDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
-		'!armor': { baseUrl: firstEditionBaseUrl, segment: 'EquipmentArmorDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
-		'!ring': { baseUrl: firstEditionBaseUrl, segment: 'MagicRingsDisplay.aspx?FinalName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
-		'!monster': { baseUrl: firstEditionBaseUrl, segment: 'MonsterDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListFeats"]' },
-		'!magicweapon': { baseUrl: firstEditionBaseUrl, segment: 'MagicWeaponsDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
-		'!condition': { baseUrl: secondEditionBaseUrl, segment: 'Conditions.aspx', selector: '#ctl00_MainContent_DetailedOutput' },
+		'!skill': { segment: 'Skills.aspx?ItemName=', selector: '#ctl00_MainContent_DataListTalentsAll' },
+		'!feat': { segment: 'FeatDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
+		'!spell': { segment: 'SpellDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
+		'!armor': { segment: 'EquipmentArmorDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
+		'!ring': { segment: 'MagicRingsDisplay.aspx?FinalName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
+		'!monster': { segment: 'MonsterDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListFeats"]' },
+		'!magicweapon': { segment: 'MagicWeaponsDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
+		'!condition': { segment: 'Conditions.aspx', selector: '#ctl00_MainContent_DetailedOutput' },
 	},
 	commandsWithSpecialProcessing: [ '!condition' ],
 	wordsNotToCapitalize: [
