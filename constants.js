0ebe5c6dd36f35f30f52a4793d7fbea1c5f8e5c6
Charles Peden <ccbpeden@warpmail.net>
2021-06-04 19:24:02 -0700

    lint constants file

--- constants.js
+++ constants.js
@@ -4,11 +4,20 @@ module.exports = {
 		'!skill': { segment: 'Skills.aspx?ItemName=', selector: '#ctl00_MainContent_DataListTalentsAll' },
 		'!feat': { segment: 'FeatDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
 		'!spell': { segment: 'SpellDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
-		'!armor': { segment: 'EquipmentArmorDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
+		'!armor': {
+			segment: 'EquipmentArmorDisplay.aspx?ItemName=',
+			selector: '[id*="ctl00_MainContent_DataListTypes"]',
+		},
 		'!ring': { segment: 'MagicRingsDisplay.aspx?FinalName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
 		'!monster': { segment: 'MonsterDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListFeats"]' },
-		'!magicweapon': { segment: 'MagicWeaponsDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
-		'!condition': { segment: 'Rules.aspx?Name=Conditions&Category=Combat', selector: '#ctl00_MainContent_RulesResult' },
+		'!magicweapon': {
+			segment: 'MagicWeaponsDisplay.aspx?ItemName=',
+			selector: '[id*="ctl00_MainContent_DataListTypes"]',
+		},
+		'!condition': {
+			segment: 'Rules.aspx?Name=Conditions&Category=Combat',
+			selector: '#ctl00_MainContent_RulesResult',
+		},
 	},
 	commandsWithSpecialProcessing: [ '!condition' ],
 	wordsNotToCapitalize: [
