66df2f36efaeba6bff5f48135bbd58d6c72e47f4
Charles Peden <ccbpeden@warpmail.net>
2021-08-15 16:22:07 -0700

    break up shinies

--- constants.js
+++ constants.js
@@ -1,15 +1,30 @@
 module.exports = {
 	defaultQueryError: 'Sorry, I can\'t match your query.',
 	pageArgs: {
-		'!skill': { segment: 'Skills.aspx?ItemName=', selector: '#ctl00_MainContent_DataListTalentsAll' },
-		'!feat': { segment: 'FeatDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
-		'!spell': { segment: 'SpellDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
+		'!skill': {
+			segment: 'Skills.aspx?ItemName=',
+			selector: '#ctl00_MainContent_DataListTalentsAll'
+		},
+		'!feat': {
+			segment: 'FeatDisplay.aspx?ItemName=',
+			selector: '[id*="ctl00_MainContent_DataListTypes"]'
+		},
+		'!spell': {
+			segment: 'SpellDisplay.aspx?ItemName=',
+			selector: '[id*="ctl00_MainContent_DataListTypes"]'
+		},
 		'!armor': {
 			segment: 'EquipmentArmorDisplay.aspx?ItemName=',
 			selector: '[id*="ctl00_MainContent_DataListTypes"]',
 		},
-		'!ring': { segment: 'MagicRingsDisplay.aspx?FinalName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
-		'!monster': { segment: 'MonsterDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListFeats"]' },
+		'!ring': {
+			segment: 'MagicRingsDisplay.aspx?FinalName=',
+			selector: '[id*="ctl00_MainContent_DataListTypes"]'
+		},
+		'!monster': {
+			segment: 'MonsterDisplay.aspx?ItemName=',
+			selector: '[id*="ctl00_MainContent_DataListFeats"]'
+		},
 		'!magicweapon': {
 			segment: 'MagicWeaponsDisplay.aspx?ItemName=',
 			selector: '[id*="ctl00_MainContent_DataListTypes"]',
@@ -18,7 +33,10 @@ module.exports = {
 			segment: 'Rules.aspx?Name=Conditions&Category=Combat',
 			selector: '#ctl00_MainContent_RulesResult',
 		},
-		'!weapon': { segment: 'EquipmentWeaponsDisplay.aspx?ItemName=', selector: '[id*="ctl00_MainContent_DataListTypes"]' },
+		'!weapon': {
+			segment: 'EquipmentWeaponsDisplay.aspx?ItemName=',
+			selector: '[id*="ctl00_MainContent_DataListTypes"]',
+		},
 	},
 	commandsWithSpecialProcessing: [ '!condition' ],
 	wordsNotToCapitalize: [
